752469270daf4ddb6de5169b5b326149bc53bdb8
Chandrbhan-Singh <149865105+Chandrbhan-Singh@users.noreply.github.com>
2024-09-14 16:52:19 +0530

    Update CitizenPlanRepository.java

--- src/main/java/in/chandrbhan/repo/CitizenPlanRepository.java
+++ src/main/java/in/chandrbhan/repo/CitizenPlanRepository.java
@@ -10,10 +10,10 @@ import in.chandrbhan.entity.CitizenPlan;
 public interface CitizenPlanRepository 
 			extends JpaRepository<CitizenPlan, Integer> {
 	
-	
+	// custome query to get plan name
 	@Query("select distinct (planName) from CitizenPlan")
 	public List<String> getPlanNames();
-	
+	// custome query to get plan status
 	@Query("select distinct (planStatus) from CitizenPlan")
 	public List<String> getPlanStatus();
 }
